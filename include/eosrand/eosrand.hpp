d270c4a9e75b184c9a38d65ba9d2e78873804935
Eunhee Jo <joeun2ce@gmail.com>
2019-06-13 18:37:55 +0900

    Under construction

--- include/eosrand/eosrand.hpp
+++ include/eosrand/eosrand.hpp
@@ -1,26 +1,66 @@
 #include <eosio/eosio.hpp>
+#include <eosio/asset.hpp>
 
 using namespace eosio;
 using namespace std;
 
 class [[eosio::contract]] eosrand : public contract {
-   public:
-      using contract::contract;
-      struct [[eosio::table]] event {
-         uint8_t random_number;
-         checksum256 seed;
-         name event_name;
-         uint32_t prize_rank;
+public:
+   using contract::contract;
 
-         uint64_t primary_key()const { return event_name.value; }
-         EOSLIB_SERIALIZE(event, (random_number)(seed)(event_name)(prize_rank))
-      };
+   [[eosio::on_notify("*::transfer")]]
+   void on_transfer(name from, name to, asset quantity, string memo);
+   typedef action_wrapper<"transfer"_n, &eosrand::on_transfer> transfer_action;
 
-      typedef multi_index<"event"_n, event> events;
+   // dummy action to resolve cdt v1.6.1 issue on notify handler
+   [[eosio::on_notify("eosio.token::transfer")]]
+   void on_eos_transfer(name from, name to, asset quantity, string memo) {
+      on_transfer(from, to, quantity, memo);
+   }
 
-      [[eosio::action]]
-      uint8_t newevent(name event_name, uint32_t prize_rank, name owner, string srv_seed);
+   [[eosio::action]]
+   checksum256 mixseed(const checksum256& sseed, checksum256& useed) const;
 
-      [[eosio::action]]
-      uint8_t srand();
+   [[eosio::action]]
+   bool hashcheck(const checksum256 hash) const;
+
+   //vector lucky box
+   //[[eosio::action]]
+   //newgacha(gamename, username, contrract_name, hash, luckybox number);
+
+   [[eosio::action]]
+   void newevent(name owner, name contract_name, name participant, extended_asset value, checksum256 oseed, time_point_sec timelock);
+
+   [[eosio::action]]
+   void setuserseed(name owner, name contract_name, name participant, checksum256 useed);
+
+   [[eosio::action]]
+   void setownseed(name owner, name contract_name, checksum256 oseed);
+
+   [[eosio::action]]
+   void withdraw(name owner, name contract_name, checksum256 preimage);
+
+   struct st_seeds {
+      checksum256 seed1;
+      checksum256 seed2;
+   };
+
+   struct [[eosio::table]] event {
+      name contract_name;
+      name participant;
+      extended_asset value;
+      checksum256 preimage;
+      checksum256 seed;
+      time_point_sec timelock;
+      bool activated;
+
+      uint64_t primary_key()const { return contract_name.value; }
+      EOSLIB_SERIALIZE(event, (contract_name)(participant)(value)(preimage)(seed)(timelock)(activated))
+   };
+
+   typedef multi_index<"event"_n, event> events;
+
+
+   //[[eosio::action]]
+   //uint8_t srand();
 };
--- src/eosrand.cpp
+++ src/eosrand.cpp
@@ -1,7 +1,112 @@
 #include <eosrand/eosrand.hpp>
 #include <eosio/crypto.hpp>
+#include <eosio/system.hpp>
 #include <eosio/transaction.hpp>
 
+using std::string;
+
+bool eosrand::hashcheck(const checksum256 hash) const {
+   auto data = hash.get_array();
+   return data[0] == 0 && data[1] == 0;
+}
+
+void eosrand::on_transfer(name from, name to, asset quantity, string memo) {
+   if (from == _self) return;
+
+   events idx(_self, from.value);
+   const auto& it = idx.get(name(memo).value);
+
+   check(!it.activated, "contract is already activated");
+   check(hashcheck(it.seed), "not setting user seed");
+   check(it.timelock < current_time_point(), "the expiration time should be in past");
+   check(it.value == extended_asset{quantity, get_first_receiver()}, "token amount not match: " + it.value.quantity.to_string() + "@" + it.value.contract.to_string());
+
+   idx.modify(it, same_payer, [&](auto& en) {
+      en.activated = true;
+   });
+}
+
+checksum256 eosrand::mixseed(const checksum256& sseed, checksum256& useed) const {
+    st_seeds seeds;
+    seeds.seed1 = sseed;
+    seeds.seed2 = useed;
+    //checksum256 result = eosio::sha256( (char *)&seeds.seed1, sizeof(seeds.seed1) * 2);
+    //print("result: ", result);
+    return eosio::sha256( (char *)&seeds.seed1, sizeof(seeds.seed1) * 2);
+}
+
+void eosrand::newevent(name owner, name contract_name, name participant, extended_asset value, checksum256 oseed, time_point_sec timelock) {
+   require_auth(owner);
+
+   events idx(_self, _self.value);
+   check(idx.find(contract_name.value) == idx.end(), "exising contract name");
+   check(timelock > current_time_point(), "the expiration time should be in the future");
+   check(participant != get_self(), "the contract itself connot be set as a participant");
+
+   auto data = oseed.extract_as_byte_array();
+   auto hash = eosio::sha256(reinterpret_cast<const char*>(data.data()), data.size());
+
+   idx.emplace(owner, [&](auto& en){
+         en.contract_name = contract_name;
+         en.participant = participant;
+         en.value = value;
+         en.preimage = hash;
+         en.timelock = timelock;
+         en.activated = false;
+   });
+}
+
+void eosrand::setuserseed(name owner, name contract_name, name participant, checksum256 useed) {
+   require_auth(participant);
+
+   events idx(_self, owner.value);
+   const auto& it = idx.get(contract_name.value);
+
+   check(it.participant == participant, "not expected participant");
+
+   idx.modify(it, same_payer, [&](auto& en){
+         en.seed = useed;
+   });
+}
+
+void eosrand::setownseed(name owner, name contract_name, checksum256 oseed) {
+   require_auth(owner);
+
+   events idx(_self, owner.value);
+   const auto& it = idx.get(contract_name.value);
+
+   check(hashcheck(it.seed), "contract not activated");
+
+   auto data = oseed.extract_as_byte_array();
+   auto hash = eosio::sha256(reinterpret_cast<const char*>(data.data()), data.size());
+   check(memcmp((const void*)it.preimage.data(), (const void*)hash.data(), 32) == 0, "invalid seed");
+
+   auto result = mixseed(it.seed, oseed);
+
+   idx.modify(owner, ram_,[&](auto& en){
+         en.seed = r_seed;
+         //en.activated = true;
+   });
+}
+
+void eosrand::withdraw(name owner, name contract_name, checksum256 preimage) {
+   events idx(_self, owner.value);
+   const auto& it = idx.get(contract_name.value);
+   check(it.activated, "contract not activated");
+
+   // `preimage` works as a key here.
+   //require_auth(it.recipient);
+
+   auto data = preimage.extract_as_byte_array();
+   auto hash = eosio::sha256(reinterpret_cast<const char*>(data.data()), data.size());
+   check(memcmp((const void*)it.preimage.data(), (const void*)hash.data(), 32) == 0, "invalid preimage");
+
+   transfer_action(it.value.contract, {{_self, "active"_n}}).send(_self, it.participant, it.value.quantity, "");
+
+   idx.erase(it);
+}
+
+/*
 uint8_t eosrand::newevent(name event_name, uint32_t prize_rank, name owner, string srv_seed){
    require_auth(owner);
 
@@ -35,3 +140,4 @@ uint8_t eosrand::srand(){
    print("[ result ] ", result," [ uint8_t result ] ", r);
    return r;
 }
+*/
